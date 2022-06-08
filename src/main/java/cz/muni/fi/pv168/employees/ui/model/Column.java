a8d651b6dac9437c5d20b25511e8512edaf38291
Paul Hrdina <qhrdina@fi.muni.cz>
2022-06-08 17:57:23 +0200

    Making Column abstract while introducing its ReadOnly and Editable (inner) subclasses

--- src/main/java/cz/muni/fi/pv168/employees/ui/model/Column.java
+++ src/main/java/cz/muni/fi/pv168/employees/ui/model/Column.java
@@ -4,38 +4,33 @@ import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 
-class Column<E, T> {
+abstract class Column<E, T> {
 
     private final String name;
     private final Function<E, T> valueGetter;
-    private final BiConsumer<E, T> valueSetter;
     private final Class<T> columnType;
 
-    private Column(String name, Class<T> columnClass, Function<E, T> valueGetter, BiConsumer<E, T> valueSetter) {
+    private Column(String name, Class<T> columnClass, Function<E, T> valueGetter) {
+        // see Item 49: Check parameters for validity
         this.name = Objects.requireNonNull(name, "name cannot be null");
         this.columnType = Objects.requireNonNull(columnClass, "column class cannot be null");
         this.valueGetter = Objects.requireNonNull(valueGetter, "value getter cannot be null");
-        this.valueSetter = valueSetter;
     }
 
     // see Item 1: Consider static factory methods instead of constructors
     public static <E, T> Column<E, T> editable(String name, Class<T> columnClass, Function<E, T> valueGetter,
                                                BiConsumer<E, T> valueSetter) {
-        return new Column<>(name, columnClass, valueGetter,
-                Objects.requireNonNull(valueSetter, "value setter cannot be null"));
+        return new Editable<>(name, columnClass, valueGetter, valueSetter);
     }
 
     // see Item 1: Consider static factory methods instead of constructors
     public static <E, T> Column<E, T> readonly(String name, Class<T> columnClass, Function<E, T> valueGetter) {
-        return new Column<>(name, columnClass, valueGetter, null);
+        return new ReadOnly<>(name, columnClass, valueGetter);
     }
 
-    void setValue(Object value, E entity) {
-        if (valueSetter == null) {
-            throw new UnsupportedOperationException("Cannot set value in readonly column: '" + name + "'");
-        }
-        valueSetter.accept(entity, columnType.cast(value)); // see Item 33: Consider type-safe heterogeneous containers
-    }
+    abstract boolean isEditable();
+
+    abstract void setValue(Object value, E entity);
 
     T getValue(E entity) {
         return valueGetter.apply(entity);
@@ -45,11 +40,44 @@ class Column<E, T> {
         return name;
     }
 
-    Class<?> getColumnType() {
+    Class<T> getColumnType() {
         return columnType;
     }
 
-    boolean isEditable() {
-        return valueSetter != null;
+    private static class ReadOnly<E, T> extends Column<E, T> {
+
+        private ReadOnly(String name, Class<T> columnClass, Function<E, T> valueGetter) {
+            super(name, columnClass, valueGetter);
+        }
+
+        @Override
+        boolean isEditable() {
+            return false;
+        }
+
+        @Override
+        void setValue(Object value, E entity) {
+            throw new UnsupportedOperationException("Column '" + getName() + "' is not editable");
+        }
+    }
+
+    private static class Editable<E, T> extends Column<E, T> {
+
+        private final BiConsumer<E, T> valueSetter;
+
+        private Editable(String name, Class<T> columnClass, Function<E, T> valueGetter, BiConsumer<E, T> valueSetter) {
+            super(name, columnClass, valueGetter);
+            this.valueSetter = Objects.requireNonNull(valueSetter, "value setter cannot be null");
+        }
+
+        @Override
+        boolean isEditable() {
+            return true;
+        }
+
+        @Override
+        void setValue(Object value, E entity) {
+            valueSetter.accept(entity, getColumnType().cast(value)); // see Item 33: Consider type-safe heterogeneous containers
+        }
     }
 }
