a32ca6ca93866138f52c1f8d8616f542bd1a74b2
Jiří Weiser <xweiser1@fi.muni.cz>
2022-10-30 21:40:41 +0100

    Introduce EmployeeTablePanel

--- src/main/java/cz/muni/fi/pv168/employees/ui/MainWindow.java
+++ src/main/java/cz/muni/fi/pv168/employees/ui/MainWindow.java
@@ -1,33 +1,22 @@
 package cz.muni.fi.pv168.employees.ui;
 
 import cz.muni.fi.pv168.employees.data.TestDataGenerator;
-import cz.muni.fi.pv168.employees.model.Department;
-import cz.muni.fi.pv168.employees.model.Employee;
-import cz.muni.fi.pv168.employees.model.Gender;
 import cz.muni.fi.pv168.employees.ui.action.AddAction;
 import cz.muni.fi.pv168.employees.ui.action.DeleteAction;
 import cz.muni.fi.pv168.employees.ui.action.EditAction;
 import cz.muni.fi.pv168.employees.ui.action.QuitAction;
-import cz.muni.fi.pv168.employees.ui.model.ComboBoxModelAdapter;
-import cz.muni.fi.pv168.employees.ui.model.EmployeeTableModel;
 import cz.muni.fi.pv168.employees.ui.model.DepartmentListModel;
+import cz.muni.fi.pv168.employees.ui.model.EmployeeTableModel;
+import cz.muni.fi.pv168.employees.ui.panels.EmployeeTablePanel;
 
 import javax.swing.Action;
-import javax.swing.DefaultCellEditor;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPopupMenu;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JToolBar;
-import javax.swing.ListModel;
-import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
-import javax.swing.event.ListSelectionEvent;
 import java.awt.BorderLayout;
-import java.util.List;
 
 public class MainWindow {
 
@@ -42,12 +31,13 @@ public class MainWindow {
         frame = createFrame();
         var testDataGenerator = new TestDataGenerator();
         var departmentListModel = new DepartmentListModel(testDataGenerator.getDepartments());
-        var employeeTable = createEmployeeTable(testDataGenerator.createTestEmployees(10), departmentListModel);
-        addAction = new AddAction(employeeTable, testDataGenerator, departmentListModel);
-        deleteAction = new DeleteAction(employeeTable);
-        editAction = new EditAction(employeeTable, departmentListModel);
-        employeeTable.setComponentPopupMenu(createEmployeeTablePopupMenu());
-        frame.add(new JScrollPane(employeeTable), BorderLayout.CENTER);
+        var employeeTableModel = new EmployeeTableModel(testDataGenerator.createTestEmployees(10));
+        var employeeTablePanel = new EmployeeTablePanel(employeeTableModel, departmentListModel, this::changeActionsState);
+        addAction = new AddAction(employeeTablePanel.getTable(), testDataGenerator, departmentListModel);
+        deleteAction = new DeleteAction(employeeTablePanel.getTable());
+        editAction = new EditAction(employeeTablePanel.getTable(), departmentListModel);
+        employeeTablePanel.setComponentPopupMenu(createEmployeeTablePopupMenu());
+        frame.add(employeeTablePanel, BorderLayout.CENTER);
         frame.add(createToolbar(), BorderLayout.BEFORE_FIRST_LINE);
         frame.setJMenuBar(createMenuBar());
         frame.pack();
@@ -62,18 +52,6 @@ public class MainWindow {
         var frame = new JFrame("Employee records");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         return frame;
-    }
-
-    private JTable createEmployeeTable(List<Employee> employees, ListModel<Department> departmentListModel) {
-        var model = new EmployeeTableModel(employees);
-        var table = new JTable(model);
-        table.setAutoCreateRowSorter(true);
-        table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
-        var genderComboBox = new JComboBox<>(Gender.values());
-        table.setDefaultEditor(Gender.class, new DefaultCellEditor(genderComboBox));
-        var departmentComboBox = new JComboBox<>(new ComboBoxModelAdapter<>(departmentListModel));
-        table.setDefaultEditor(Department.class, new DefaultCellEditor(departmentComboBox));
-        return table;
     }
 
     private JPopupMenu createEmployeeTablePopupMenu() {
@@ -105,12 +83,6 @@ public class MainWindow {
         toolbar.add(editAction);
         toolbar.add(deleteAction);
         return toolbar;
-    }
-
-    private void rowSelectionChanged(ListSelectionEvent listSelectionEvent) {
-        var selectionModel = (ListSelectionModel) listSelectionEvent.getSource();
-        var count = selectionModel.getSelectedItemsCount();
-        changeActionsState(count);
     }
 
     private void changeActionsState(int selectedItemsCount) {
---
+++ src/main/java/cz/muni/fi/pv168/employees/ui/panels/EmployeeTablePanel.java
@@ -0,0 +1,59 @@
+package cz.muni.fi.pv168.employees.ui.panels;
+
+import cz.muni.fi.pv168.employees.model.Department;
+import cz.muni.fi.pv168.employees.model.Gender;
+import cz.muni.fi.pv168.employees.ui.model.ComboBoxModelAdapter;
+import cz.muni.fi.pv168.employees.ui.model.DepartmentListModel;
+import cz.muni.fi.pv168.employees.ui.model.EmployeeTableModel;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import java.awt.BorderLayout;
+import java.util.function.Consumer;
+
+/**
+ * Panel with employee records in a table.
+ */
+public class EmployeeTablePanel extends JPanel {
+
+    private final JTable table;
+    private final Consumer<Integer> onSelectionChange;
+
+    public EmployeeTablePanel(EmployeeTableModel employeeTableModel, DepartmentListModel departmentListModel, Consumer<Integer> onSelectionChange) {
+        setLayout(new BorderLayout());
+        table = setUpTable(employeeTableModel, departmentListModel);
+        add(new JScrollPane(table), BorderLayout.CENTER);
+
+        this.onSelectionChange = onSelectionChange;
+    }
+
+    public JTable getTable() {
+        return table;
+    }
+
+    private JTable setUpTable(EmployeeTableModel employeeTableModel, DepartmentListModel departmentListModel) {
+        var table = new JTable(employeeTableModel);
+
+        table.setAutoCreateRowSorter(true);
+        table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
+        var genderComboBox = new JComboBox<>(Gender.values());
+        table.setDefaultEditor(Gender.class, new DefaultCellEditor(genderComboBox));
+        var departmentComboBox = new JComboBox<>(new ComboBoxModelAdapter<>(departmentListModel));
+        table.setDefaultEditor(Department.class, new DefaultCellEditor(departmentComboBox));
+
+        return table;
+    }
+
+    private void rowSelectionChanged(ListSelectionEvent listSelectionEvent) {
+        var selectionModel = (ListSelectionModel) listSelectionEvent.getSource();
+        var count = selectionModel.getSelectedItemsCount();
+        if (onSelectionChange != null) {
+            onSelectionChange.accept(count);
+        }
+    }
+}
