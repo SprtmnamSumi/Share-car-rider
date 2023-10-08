ce7a47d9b24d528cd0d3702c981066984ec50d39
Jan Smid <xsmid2@fi.muni.cz>
2023-10-08 12:16:57 +0200

    #6 Grid + Example row

--- src/main/java/cz/muni/fi/pv168/project/gui/table/RideTable.java
+++ src/main/java/cz/muni/fi/pv168/project/gui/table/RideTable.java
@@ -1,9 +1,11 @@
 package cz.muni.fi.pv168.project.gui.table;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,10 +20,12 @@ public class RideTable extends JTable {
         super();
 
         // Set look
-        this.setGridColor(Color.RED);
+        this.setGridColor(Color.GRAY);
+        this.setBackground(Color.WHITE);
         this.setShowGrid(true);
         this.setPreferredSize(new Dimension(1920,1080));
 
+        /*
         // Set up columns
         dateColumn.setHeaderValue("Date");
         this.addColumn(dateColumn);
@@ -31,11 +35,20 @@ public class RideTable extends JTable {
         this.addColumn(distanceColumn);
         categoryColumn.setHeaderValue("Category");
         this.addColumn(categoryColumn);
+        */
+
+        String[] tableHead={"Date","Name","Distance","Category"};
+        DefaultTableModel tableModel=new DefaultTableModel(tableHead,0);
+        this.setModel(tableModel);
+
+        // Testing table row
+        String[] item={"1.1.2023","Zaphod Beeblebrox","42","The starship Heart of Gold"};
+        tableModel.addRow(item);
 
         // Add context menu
         JPopupMenu popupMenu = new JPopupMenu();
-        popupMenu.add(new JMenuItem("Option 1"));
-        popupMenu.add(new JMenuItem("Option 2"));
+        popupMenu.add(new JMenuItem("Edit"));
+        popupMenu.add(new JMenuItem("Delete"));
         this.setComponentPopupMenu(popupMenu);
     }
 }
