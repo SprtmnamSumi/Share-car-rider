9665d2cdad290190c117e2fbed220fa9a12ce81b
ManasComp <OndrejMan1@gmail.com>
2023-10-29 20:41:30 +0100

    Connect dialogs

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/CarRideDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/CarRideDialog.java
@@ -4,38 +4,33 @@ import cz.muni.fi.pv168.project.business.model.CarRide;
 import cz.muni.fi.pv168.project.business.model.Category;
 import cz.muni.fi.pv168.project.business.model.Currency;
 import cz.muni.fi.pv168.project.business.model.Template;
-import cz.muni.fi.pv168.project.ui.model.LocalDateModel;
 import cz.muni.fi.pv168.project.ui.model.adapters.ComboBoxModelAdapter;
-import cz.muni.fi.pv168.project.ui.panels.commonPanels.CostBar;
 import cz.muni.fi.pv168.project.ui.panels.commonPanels.DateBar;
-import org.jdatepicker.DateModel;
 
 import javax.swing.*;
-import java.time.LocalDate;
 
 public final class CarRideDialog extends EntityDialog<CarRide> {
-
     private final JTextField titleField = new JTextField();
     private final JTextField descriptionField = new JTextField();
     private final JTextField templateField = new JTextField();
     private final ComboBoxModel<Currency> currencyModel = new DefaultComboBoxModel<>(Currency.values());
-    private final ComboBoxModel<Template> categoryMmodel;
+    private final JComboBox<Template> templateComboBoxModel;
+    private final JComboBox<Category> categoryJComboBox;
     private final JSpinner rateField = new JSpinner(new SpinnerNumberModel());
     private final JSpinner distanceField = new JSpinner(new SpinnerNumberModel());
     private final JSpinner fuelConsiumption = new JSpinner(new SpinnerNumberModel());
     private final JSpinner costOfFuel = new JSpinner(new SpinnerNumberModel());
     private final JSpinner numberOfPassengers = new JSpinner(new SpinnerNumberModel());
     private final JSpinner commision = new JSpinner(new SpinnerNumberModel());
-
-    private final ComboBoxModel<Category> categoryModel;
-    private final DateModel<LocalDate> dateField = new LocalDateModel();
-
+    private final JCheckBox isChecked = new JCheckBox();
+    private final DateBar dateBar = new DateBar();
     private final CarRide carRide;
 
     public CarRideDialog(CarRide carRide, ListModel<Category> categoryModel, ListModel<Template> templateModel) {
         this.carRide = carRide;
-        this.categoryModel = new ComboBoxModelAdapter<>(categoryModel);
-        this.categoryMmodel = new ComboBoxModelAdapter<>((templateModel));
+
+        templateComboBoxModel = new JComboBox<>(new ComboBoxModelAdapter<>(templateModel));
+        categoryJComboBox = new JComboBox<>(new ComboBoxModelAdapter<>(categoryModel));
         setValues();
         addFields();
     }
@@ -43,32 +38,49 @@ public final class CarRideDialog extends EntityDialog<CarRide> {
     private void setValues() {
         titleField.setText(carRide.getTitle());
         descriptionField.setText(carRide.getDescription());
-        categoryModel.setSelectedItem(carRide.getCategory());
-//        dateField.setValue(carRide.getDate());
+        distanceField.setValue(carRide.getDistance());
+        fuelConsiumption.setValue(carRide.getFuelConsumption());
+        costOfFuel.setValue(carRide.getCostOfFuelPerLitre());
+        numberOfPassengers.setValue(carRide.getNumberOfPassengers());
+        commision.setValue(carRide.getCommission());
+        categoryJComboBox.setSelectedItem(carRide.getCategory());
+        dateBar.setDate(carRide.getDate());
+    }
+
+    private <T> T getSpinnerValue(JSpinner spinner) {
+        try {
+            spinner.commitEdit();
+        } catch (java.text.ParseException e) {
+        }
+        T value = (T) spinner.getValue();
+        return value;
     }
 
     private void addFields() {
-        add("Template", new JComboBox<>(categoryMmodel));
-        add("Title", templateField);
+        add("Template", templateComboBoxModel);
+        add("Title", titleField);
         add("Description", descriptionField);
         add("Distance", distanceField);
         add("Average Fuel Consumption (per 100km)", fuelConsiumption);
-        add("Cost of Fuel (1l)", new CostBar());
+        add("Cost of Fuel (1l)", costOfFuel);
         add("Number of Passengers", numberOfPassengers);
         add("Commision (%)", commision);
-
-        add("Date", new DateBar(dateField));
-        add("Category", new JComboBox<>(categoryModel));
-        add("Count me in the calculation of per price person", new JCheckBox());
+        add("Date", dateBar);
+        add("Category", categoryJComboBox);
+        add("Count me in the calculation of per price person", isChecked);
     }
 
     @Override
     CarRide getEntity() {
-//        carRide.setFirstName(titleField.getText());
-//        carRide.setLastName(descriptionField.getText());
-//        carRide.setGender((Gender) genderModel.getSelectedItem());
-//        carRide.setDepartment((Department) departmentModel.getSelectedItem());
-//        carRide.setBirthDate(dateField.getValue());
+        carRide.setTitle(titleField.getText());
+        carRide.setDescription(descriptionField.getText());
+//        carRide.setDistance(getSpinnerValue(distanceField));
+//        fuelConsiumption.setValue(getSpinnerValue(fuelConsiumption));
+//        carRide.setCostOfFuelPerLitre(getSpinnerValue(costOfFuel));
+        carRide.setNumberOfPassengers(getSpinnerValue(numberOfPassengers));
+//        carRide.setCommission(getSpinnerValue(commision));
+        carRide.setCategory((Category) categoryJComboBox.getSelectedItem());
+        carRide.setDate(dateBar.getDate());
         return carRide;
     }
 }
--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/TemplateDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/TemplateDialog.java
@@ -3,13 +3,9 @@ package cz.muni.fi.pv168.project.ui.dialog;
 import cz.muni.fi.pv168.project.business.model.Category;
 import cz.muni.fi.pv168.project.business.model.Currency;
 import cz.muni.fi.pv168.project.business.model.Template;
-import cz.muni.fi.pv168.project.ui.model.LocalDateModel;
 import cz.muni.fi.pv168.project.ui.model.adapters.ComboBoxModelAdapter;
-import cz.muni.fi.pv168.project.ui.panels.commonPanels.CostBar;
-import org.jdatepicker.DateModel;
 
 import javax.swing.*;
-import java.time.LocalDate;
 
 public final class TemplateDialog extends EntityDialog<Template> {
 
@@ -17,23 +13,24 @@ public final class TemplateDialog extends EntityDialog<Template> {
     private final JTextField descriptionField = new JTextField();
     private final JTextField templateField = new JTextField();
     private final ComboBoxModel<Currency> currencyModel = new DefaultComboBoxModel<>(Currency.values());
-    private final ComboBoxModel<Template> categoryMmodel;
+    private final JComboBox<Template> templateComboBoxModel;
+    private final JComboBox<Category> categoryJComboBox;
     private final JSpinner rateField = new JSpinner(new SpinnerNumberModel());
     private final JSpinner distanceField = new JSpinner(new SpinnerNumberModel());
     private final JSpinner fuelConsiumption = new JSpinner(new SpinnerNumberModel());
     private final JSpinner costOfFuel = new JSpinner(new SpinnerNumberModel());
     private final JSpinner numberOfPassengers = new JSpinner(new SpinnerNumberModel());
     private final JSpinner commision = new JSpinner(new SpinnerNumberModel());
+    private final JCheckBox isChecked = new JCheckBox();
 
-    private final ComboBoxModel<Category> categoryModel;
-    private final DateModel<LocalDate> dateField = new LocalDateModel();
 
     private final Template template;
 
     public TemplateDialog(Template template, ListModel<Category> categoryModel, ListModel<Template> templateModel) {
         this.template = template;
-        this.categoryModel = new ComboBoxModelAdapter<>(categoryModel);
-        this.categoryMmodel = new ComboBoxModelAdapter<>((templateModel));
+
+        templateComboBoxModel = new JComboBox<>(new ComboBoxModelAdapter<>(templateModel));
+        categoryJComboBox = new JComboBox<>(new ComboBoxModelAdapter<>(categoryModel));
         setValues();
         addFields();
     }
@@ -41,31 +38,46 @@ public final class TemplateDialog extends EntityDialog<Template> {
     private void setValues() {
         titleField.setText(template.getTitle());
         descriptionField.setText(template.getDescription());
-
-        categoryModel.setSelectedItem(template.getCategory());
-//        dateField.setValue(template.getDate());
+        distanceField.setValue(template.getDistance());
+        fuelConsiumption.setValue(template.getFuelConsumption());
+        costOfFuel.setValue(template.getCostOfFuelPerLitre());
+        numberOfPassengers.setValue(template.getNumberOfPassengers());
+        commision.setValue(template.getCommission());
+        categoryJComboBox.setSelectedItem(template.getCategory());
     }
 
     private void addFields() {
-        add("Template", new JComboBox<>(categoryMmodel));
-        add("Title", templateField);
+        add("Template", templateComboBoxModel);
+        add("Title", titleField);
         add("Description", descriptionField);
         add("Distance", distanceField);
         add("Average Fuel Consumption (per 100km)", fuelConsiumption);
-        add("Cost of Fuel (1l)", new CostBar());
+        add("Cost of Fuel (1l)", costOfFuel);
         add("Number of Passengers", numberOfPassengers);
         add("Commision (%)", commision);
-        add("Category", new JComboBox<>(categoryModel));
-        add("Count me in the calculation of per price person", new JCheckBox());
+        add("Category", categoryJComboBox);
+        add("Count me in the calculation of per price person", isChecked);
+    }
+
+    private <T> T getSpinnerValue(JSpinner spinner) {
+        try {
+            spinner.commitEdit();
+        } catch (java.text.ParseException e) {
+        }
+        T value = (T) spinner.getValue();
+        return value;
     }
 
     @Override
     Template getEntity() {
-//        template.setFirstName(titleField.getText());
-//        template.setLastName(descriptionField.getText());
-//        template.setGender((Gender) genderModel.getSelectedItem());
-//        template.setDepartment((Department) departmentModel.getSelectedItem());
-//        template.setBirthDate(dateField.getValue());
+        template.setTitle(titleField.getText());
+        template.setDescription(descriptionField.getText());
+//        template.setDistance(getSpinnerValue(distanceField));
+//        fuelConsiumption.setValue(getSpinnerValue(fuelConsiumption));
+//        template.setCostOfFuelPerLitre(getSpinnerValue(costOfFuel));
+        template.setNumberOfPassengers(getSpinnerValue(numberOfPassengers));
+//        template.setCommission(getSpinnerValue(commision));
+        template.setCategory((Category) categoryJComboBox.getSelectedItem());
         return template;
     }
 }
