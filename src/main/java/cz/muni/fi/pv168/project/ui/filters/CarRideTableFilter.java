9bb219a75e01ab2cb76d9ddb1fa22089d9e662fa
marvav <41584620+marvav@users.noreply.github.com>
2023-11-07 08:18:53 +0100

    Added filters

--- src/main/java/cz/muni/fi/pv168/project/ui/filters/CarRideTableFilter.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/filters/CarRideTableFilter.java
@@ -1,11 +1,11 @@
 package cz.muni.fi.pv168.project.ui.filters;
 
 import cz.muni.fi.pv168.project.business.model.CarRide;
-import cz.muni.fi.pv168.project.ui.filters.matchers.EntityMatcher;
-import cz.muni.fi.pv168.project.ui.filters.matchers.EntityMatchers;
+import cz.muni.fi.pv168.project.ui.filters.matchers.*;
 import cz.muni.fi.pv168.project.ui.model.CarRide.CarRideTableModel;
 
 import javax.swing.table.TableRowSorter;
+import java.util.Date;
 import java.util.stream.Stream;
 
 
@@ -17,20 +17,60 @@ public final class CarRideTableFilter {
         rowSorter.setRowFilter(rideCompoundMatcher);
     }
 
+    public void filterByTime(Date fromDate, Date toDate) {
+        rideCompoundMatcher.setDateMatcher(new DateIntervalMatcher(fromDate, toDate));
+    }
+
+    public void filterByDistance(int fromDistance, int toDistance) {
+        rideCompoundMatcher.setDistanceMatcher(new DistanceIntervalMatcher(fromDistance, toDistance));
+    }
+
+    public void filterByPassengers(int passengers) {
+        rideCompoundMatcher.setPassengerMatcher(new PassengersCountMatcher(passengers));
+    }
+
+    public void removePassengersFilter() {
+        rideCompoundMatcher.setPassengerMatcher(EntityMatchers.all());
+    }
+    public void removeDistanceFilter() {
+        rideCompoundMatcher.setDistanceMatcher(EntityMatchers.all());
+    }
+
+    public void removeDateFilter() {
+        rideCompoundMatcher.setDateMatcher(EntityMatchers.all());
+    }
+
 
     private static class RideCompoundMatcher extends EntityMatcher<CarRide> {
 
         private final TableRowSorter<CarRideTableModel> rowSorter;
-        private final EntityMatcher<CarRide> genderMatcher = EntityMatchers.all();
+        private EntityMatcher<CarRide> dateMatcher = EntityMatchers.all();
+        private EntityMatcher<CarRide> passengerMatcher = EntityMatchers.all();
+        private EntityMatcher<CarRide> distanceMatcher = EntityMatchers.all();
 
         private RideCompoundMatcher(TableRowSorter<CarRideTableModel> rowSorter) {
             this.rowSorter = rowSorter;
         }
 
+        private void setDistanceMatcher(EntityMatcher<CarRide> distanceMatcher) {
+            this.distanceMatcher = distanceMatcher;
+            rowSorter.sort();
+        }
+
+        private void setPassengerMatcher(EntityMatcher<CarRide> passengerMatcher) {
+            this.passengerMatcher = passengerMatcher;
+            rowSorter.sort();
+        }
+
+        private void setDateMatcher(EntityMatcher<CarRide> dateMatcher) {
+            this.dateMatcher = dateMatcher;
+            rowSorter.sort();
+        }
+
+
         @Override
         public boolean evaluate(CarRide carRide) {
-            return Stream.of(genderMatcher)
-                    .allMatch(m -> m.evaluate(carRide));
+            return Stream.of(dateMatcher, distanceMatcher, passengerMatcher).allMatch(m -> m.evaluate(carRide));
         }
     }
 }
