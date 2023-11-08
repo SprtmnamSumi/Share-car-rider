12ec8c9b7b304428c6a00a8b07a38f5f46118222
Marek Horský <xhorsky@fi.muni.cz>
2023-11-08 07:32:57 +0100

    Car 51 disable cell edit

--- src/main/java/cz/muni/fi/pv168/project/ui/panels/Category/CategoryTablePanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/Category/CategoryTablePanel.java
@@ -3,8 +3,8 @@ package cz.muni.fi.pv168.project.ui.panels.Category;
 import cz.muni.fi.pv168.project.business.model.Category;
 import cz.muni.fi.pv168.project.ui.action.DefaultActionFactory;
 import cz.muni.fi.pv168.project.ui.model.Category.CategoryTableModel;
+import cz.muni.fi.pv168.project.ui.panels.TablePanel;
 
-import javax.inject.Inject;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import java.awt.*;
@@ -13,31 +13,22 @@ import java.util.function.Consumer;
 /**
  * Panel with category records in a table.
  */
-public class CategoryTablePanel extends JPanel {
+public class CategoryTablePanel extends TablePanel<Category> {
 
-    private final JTable table;
     private final Consumer<Integer> onSelectionChange;
     private Action addCategoryAction;
     private Action editCategoryAction;
     private Action deleteCategoryAction;
 
-    @Inject
     public CategoryTablePanel(CategoryTableModel categoryTableModel, DefaultActionFactory<Category> actionFactory) {
-        setLayout(new BorderLayout());
-        table = setUpTable(categoryTableModel, actionFactory);
+        super(categoryTableModel);
+        setUpTable(actionFactory);
         add(new JScrollPane(table), BorderLayout.CENTER);
 
         this.onSelectionChange = this::changeActionsState;
     }
 
-    public JTable getTable() {
-        return table;
-    }
-
-    private JTable setUpTable(CategoryTableModel categoryTableModel, DefaultActionFactory<Category> actionFactory) {
-        var table = new JTable(categoryTableModel);
-
-        table.setAutoCreateRowSorter(true);
+    private void setUpTable(DefaultActionFactory<Category> actionFactory) {
         table.getSelectionModel().addListSelectionListener(this::rowSelectionChanged);
 
         addCategoryAction = actionFactory.getAddAction(table);
@@ -45,9 +36,6 @@ public class CategoryTablePanel extends JPanel {
         deleteCategoryAction = actionFactory.getDeleteAction(table);
 
         table.setComponentPopupMenu(createCategoryTablePopUpMenu());
-
-
-        return table;
     }
 
     private JPopupMenu createCategoryTablePopUpMenu() {
