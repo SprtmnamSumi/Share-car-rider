a4ccb20d83a8928ccf2f233c9c707c5fe163d038
Ondřej Man <xman@fi.muni.cz>
2023-11-09 20:14:28 +0100

    [CAR-48] Create category in a dialog

--- src/main/java/cz/muni/fi/pv168/project/ui/action/CarRide/AddCarRideAction.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/action/CarRide/AddCarRideAction.java
@@ -4,8 +4,10 @@ package cz.muni.fi.pv168.project.ui.action.CarRide;
 import cz.muni.fi.pv168.project.business.model.Category;
 import cz.muni.fi.pv168.project.business.model.Currency;
 import cz.muni.fi.pv168.project.business.model.Template;
+import cz.muni.fi.pv168.project.ui.action.DefaultActionFactory;
 import cz.muni.fi.pv168.project.ui.dialog.CarRideDialog;
 import cz.muni.fi.pv168.project.ui.model.CarRide.CarRideTableModel;
+import cz.muni.fi.pv168.project.ui.model.Category.CategoryTableModel;
 import cz.muni.fi.pv168.project.ui.model.TableModel;
 import cz.muni.fi.pv168.project.ui.resources.Icons;
 
@@ -21,15 +23,19 @@ final class AddCarRideAction extends AbstractAction {
     private final ListModel<Currency> currencyListModel;
     private final ListModel<Template> carRideTemplateListModel;
     private final TableModel<Template> repository;
+    private final DefaultActionFactory<Category> categoryActionFactory;
+    private final CategoryTableModel categoryTableMode;
 
 
-    AddCarRideAction(JTable carRidesTable, ListModel<Category> categoriestListModel, ListModel<Currency> currencyListModel, ListModel<Template> carRideTemplateListModel, TableModel<Template> repository) {
+    AddCarRideAction(JTable carRidesTable, ListModel<Category> categoriestListModel, ListModel<Currency> currencyListModel, ListModel<Template> carRideTemplateListModel, TableModel<Template> repository, DefaultActionFactory<Category> categoryActionFactory, CategoryTableModel categoryTableMode) {
         super("Add", Icons.ADD_ICON);
         this.carRidesTable = carRidesTable;
         this.currencyListModel = currencyListModel;
         this.categoriestListModel = categoriestListModel;
         this.carRideTemplateListModel = carRideTemplateListModel;
         this.repository = repository;
+        this.categoryActionFactory = categoryActionFactory;
+        this.categoryTableMode = categoryTableMode;
         putValue(SHORT_DESCRIPTION, "Adds new Ride");
         putValue(MNEMONIC_KEY, KeyEvent.VK_A);
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("ctrl N"));
@@ -39,8 +45,7 @@ final class AddCarRideAction extends AbstractAction {
     public void actionPerformed(ActionEvent e) {
         var carRidesTableModel = (CarRideTableModel) carRidesTable.getModel();
 
-        var dialog = new CarRideDialog(categoriestListModel, currencyListModel, carRideTemplateListModel, repository);
-
+        var dialog = new CarRideDialog(categoriestListModel, currencyListModel, carRideTemplateListModel, repository, categoryActionFactory, categoryTableMode);
         dialog.show(carRidesTable, "Add Cat ride")
                 .ifPresent(carRidesTableModel::addRow);
     }
