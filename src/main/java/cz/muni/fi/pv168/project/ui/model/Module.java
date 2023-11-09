20c9ba3752e391f4484b6130ff5e659df28cbf56
ManasComp <OndrejMan1@gmail.com>
2023-11-09 14:02:19 +0100

    [CAR-50] Patch

--- src/main/java/cz/muni/fi/pv168/project/ui/model/Module.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/Module.java
@@ -10,37 +10,45 @@ import cz.muni.fi.pv168.project.business.model.Currency;
 import cz.muni.fi.pv168.project.business.model.Template;
 import cz.muni.fi.pv168.project.ui.model.CarRide.CarRideTableModel;
 import cz.muni.fi.pv168.project.ui.model.Category.CategoryTableModel;
-import cz.muni.fi.pv168.project.ui.model.Currency.CurrencyListModel;
 import cz.muni.fi.pv168.project.ui.model.Currency.CurrencyTableModel;
 import cz.muni.fi.pv168.project.ui.model.Template.TemplateTableModel;
 import cz.muni.fi.pv168.project.ui.model.adapters.EntityListModelAdapter;
 
-import java.util.List;
-
 public class Module extends AbstractModule {
     @Override
     protected void configure() {
-        bind(new TypeLiteral<TableModel<Category>>(){}).to(CategoryTableModel.class);
-        bind(new TypeLiteral<TableModel<CarRide>>(){}).to(CarRideTableModel.class);
-        bind(new TypeLiteral<TableModel<Template>>(){}).to(TemplateTableModel.class);
-        bind(new TypeLiteral<TableModel<Currency>>(){}).to(CurrencyTableModel.class);
+        bind(new TypeLiteral<TableModel<Category>>() {
+        }).to(CategoryTableModel.class);
+        bind(new TypeLiteral<TableModel<CarRide>>() {
+        }).to(CarRideTableModel.class);
+        bind(new TypeLiteral<TableModel<Template>>() {
+        }).to(TemplateTableModel.class);
 
-        bind(new TypeLiteral<ListModel<Currency>>(){}).to(CurrencyListModel.class);
-        //bind(new TypeLiteral<List<Currency>>(){}).to((Class<? extends List<Currency>>) List.class);
+        bind(new TypeLiteral<TableModel<Currency>>() {
+        }).to(CurrencyTableModel.class);
     }
 
-    @Provides @Singleton
+    @Provides
+    @Singleton
     EntityListModelAdapter<CarRide> getCarRideModelAdapter(TableModel<CarRide> carRideTableModel) {
         return new EntityListModelAdapter<>(carRideTableModel);
     }
 
-    @Provides @Singleton
+    @Provides
+    @Singleton
     EntityListModelAdapter<Category> getCategoryModelAdapter(TableModel<Category> categoryTableModel) {
         return new EntityListModelAdapter<>(categoryTableModel);
     }
 
-    @Provides @Singleton
+    @Provides
+    @Singleton
     EntityListModelAdapter<Template> getTemplateModelAdapter(TableModel<Template> templateTableModel) {
         return new EntityListModelAdapter<>(templateTableModel);
+    }
+
+    @Provides
+    @Singleton
+    EntityListModelAdapter<Currency> getCurrencyModelAdapter(TableModel<Currency> carRideTableModel) {
+        return new EntityListModelAdapter<>(carRideTableModel);
     }
 }
