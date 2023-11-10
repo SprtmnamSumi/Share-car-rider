030458ff0e5e6c6a3a085259307a7056b2ee2243
marvav <41584620+marvav@users.noreply.github.com>
2023-11-10 23:41:00 +0100

    Fixed Template bug

--- src/main/java/cz/muni/fi/pv168/project/ui/model/validation/FieldConversionUtils.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/model/validation/FieldConversionUtils.java
@@ -18,7 +18,7 @@ public class FieldConversionUtils {
             Integer.parseInt(field.getText());
             return true;
         }
-        catch(IllegalArgumentException e){
+        catch(NumberFormatException e){
             return false;
         }
     }
