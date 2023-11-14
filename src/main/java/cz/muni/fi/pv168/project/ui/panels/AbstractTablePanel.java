61cfb70ef0859ee9f930b14d3cbd4b23f4a338e7
marvav <41584620+marvav@users.noreply.github.com>
2023-11-14 19:32:10 +0100

    Added dynamic columns

--- src/main/java/cz/muni/fi/pv168/project/ui/panels/AbstractTablePanel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/panels/AbstractTablePanel.java
@@ -1,10 +1,14 @@
 package cz.muni.fi.pv168.project.ui.panels;
 
+import cz.muni.fi.pv168.project.ui.model.Column;
 import cz.muni.fi.pv168.project.ui.model.TableModel;
 
 import javax.swing.*;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
+import java.util.Iterator;
 
 public abstract class AbstractTablePanel extends JPanel {
 
@@ -29,5 +33,36 @@ public abstract class AbstractTablePanel extends JPanel {
         setLayout(new BorderLayout());
         table.getRowSorter().toggleSortOrder(0);
         table.getRowSorter().toggleSortOrder(0);
+        table.getTableHeader().setComponentPopupMenu(columnPopupMenu(table.getColumnModel()));
+    }
+
+    private JPopupMenu columnPopupMenu(TableColumnModel columnModel) {
+        JPopupMenu menu = new JPopupMenu();
+        Iterator<TableColumn> columns = columnModel.getColumns().asIterator();
+        while(columns.hasNext()){
+            TableColumn column = columns.next();
+            JButton menuItem = new JButton((String)column.getHeaderValue());
+            menuItem.setBackground(Color.green);
+            menuItem.addActionListener((a)->{
+                if(column.getMinWidth()==0){
+                    menuItem.setBackground(Color.green);
+                    showColumn(column);
+                }
+                else{
+                    hideColumn(column);
+                    menuItem.setBackground(Color.red);
+                }
+            });
+            menu.add(menuItem);
+        }
+        return menu;
+    }
+    private void showColumn(TableColumn column){
+        column.setMaxWidth(1000);
+        column.setMinWidth(200);
+    }
+    private void hideColumn(TableColumn column){
+        column.setMinWidth(0);
+        column.setMaxWidth(0);
     }
 }
