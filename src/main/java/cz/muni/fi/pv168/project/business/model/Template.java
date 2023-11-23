ce9e2a2d4d8a06e640b0bb909e384ab3d50dbe33
ManasComp <OndrejMan1@gmail.com>
2023-11-23 20:33:16 +0100

    Temlate equals

--- src/main/java/cz/muni/fi/pv168/project/business/model/Template.java
+++ src/main/java/cz/muni/fi/pv168/project/business/model/Template.java
@@ -120,26 +120,16 @@ public class Template extends Entity {
                 && Objects.equals(category, template.category)
                 && Objects.equals(distance, template.distance)
                 && Objects.equals(fuelConsumption, template.fuelConsumption)
-//                && Objects.equals(costOfFuelPerLitre, template.costOfFuelPerLitre)
+                && Objects.equals(costOfFuelPerLitre, template.costOfFuelPerLitre)
                 && Objects.equals(numberOfPassengers, template.numberOfPassengers)
                 && Objects.equals(commission, template.commission)
                 && Objects.equals(currency, template.currency)
-                && Objects.equals(newestConversionRateToDollar, template.newestConversionRateToDollar)
-                ;
-
-//        return Double.compare(template.distance, distance) == 0 &&
-//                Double.compare(template.fuelConsumption, fuelConsumption) == 0 &&
-//                Double.compare(template.costOfFuelPerLitre, costOfFuelPerLitre) == 0 &&
-//                numberOfPassengers == template.numberOfPassengers &&
-//                Double.compare(template.commission, commission) == 0 &&
-//                Objects.equals(title, template.title) &&
-//                Objects.equals(description, template.description) &&
-//                Objects.equals(category, template.category);
+                && Objects.equals(newestConversionRateToDollar, template.newestConversionRateToDollar);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(distance, fuelConsumption, costOfFuelPerLitre, numberOfPassengers, commission, title, description, category);
+        return Objects.hash(title, description, category, distance, fuelConsumption, costOfFuelPerLitre, numberOfPassengers, commission, currency, newestConversionRateToDollar);
     }
 
 
