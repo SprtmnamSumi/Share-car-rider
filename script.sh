e628dae1e8f2b2e297933eb8be475001ea03b20e
ManasComp <OndrejMan1@gmail.com>
2023-11-30 18:27:39 +0100

    delete script

--- script.sh
+++
@@ -1,5 +0,0 @@
-target=~/Documents/pv-168-project-share-car-rider/target/classes/cz/muni/fi/pv168/project/storage/sql/db/init.sql
-
-from=/Users/ondrejman/Documents/pv-168-project-share-car-rider/src/main/resources/cz.muni.fi.pv168.project/storage.sql.db/init.sql
-echo $from
-cp $from $target
