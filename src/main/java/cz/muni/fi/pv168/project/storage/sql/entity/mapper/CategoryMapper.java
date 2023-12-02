ecc8d394a7310241945e1e9e4510f9fbbada6d06
ManasComp <OndrejMan1@gmail.com>
2023-12-02 21:17:24 +0100

    category refactoring

--- src/main/java/cz/muni/fi/pv168/project/storage/sql/entity/mapper/CategoryMapper.java
+++ src/main/java/cz/muni/fi/pv168/project/storage/sql/entity/mapper/CategoryMapper.java
@@ -7,17 +7,7 @@ import cz.muni.fi.pv168.project.storage.sql.entity.CategoryEntity;
 /**
  * Mapper from the {@link CategoryEntity} to {@link Category}.
  */
-public final class CategoryMapper implements EntityMapper<CategoryEntity, Category> {
-
-    private static CategoryEntity getCategoryEntity(Category entity, Long dbId) {
-        return new CategoryEntity(
-                dbId,
-                entity.getGuid(),
-                entity.getName(),
-                entity.getColour()
-        );
-    }
-
+public final class CategoryMapper extends Mapper<CategoryEntity, Category> implements EntityMapper<CategoryEntity, Category> {
     @Override
     public Category mapToBusiness(CategoryEntity dbCategory) {
         return new Category(
@@ -28,12 +18,12 @@ public final class CategoryMapper implements EntityMapper<CategoryEntity, Catego
     }
 
     @Override
-    public CategoryEntity mapNewEntityToDatabase(Category entity) {
-        return getCategoryEntity(entity, null);
-    }
-
-    @Override
-    public CategoryEntity mapExistingEntityToDatabase(Category entity, Long dbId) {
-        return getCategoryEntity(entity, dbId);
+    CategoryEntity getEntity(Category entity, Long dbID) {
+        return new CategoryEntity(
+                dbID,
+                entity.getGuid(),
+                entity.getName(),
+                entity.getColour()
+        );
     }
 }
