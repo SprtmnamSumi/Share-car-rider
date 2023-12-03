26e37a21c48578b378893bee3bbd4b83e69136c5
ManasComp <OndrejMan1@gmail.com>
2023-12-03 12:35:44 +0100

    initilization

---
+++ src/main/java/cz/muni/fi/pv168/project/data/IInitializator.java
@@ -0,0 +1,5 @@
+package cz.muni.fi.pv168.project.data;
+
+public interface IInitializator {
+    void initialize(int count);
+}
--- src/main/java/cz/muni/fi/pv168/project/data/Initializator.java
+++ src/main/java/cz/muni/fi/pv168/project/data/Initializator.java
@@ -6,30 +6,30 @@ import cz.muni.fi.pv168.project.business.model.Currency;
 import cz.muni.fi.pv168.project.business.model.GuidProvider;
 import cz.muni.fi.pv168.project.business.model.Template;
 import cz.muni.fi.pv168.project.ui.model.TableModel;
-
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import java.util.List;
 
 @Singleton
-public class Initializator {
+public class Initializator implements IInitializator {
     private final TableModel<Category> categories;
     private final TableModel<CarRide> rides;
     private final TableModel<Currency> currencies;
     private final TableModel<Template> templates;
-    private final int count;
     TestDataGenerator generator;
 
-    public Initializator(GuidProvider guidProvider, TableModel<Category> categories, TableModel<CarRide> rides, TableModel<Currency> currencies, TableModel<Template> templates, int count) {
+    @Inject
+    public Initializator(GuidProvider guidProvider, TableModel<Category> categories, TableModel<CarRide> rides, TableModel<Currency> currencies, TableModel<Template> templates) {
         this.categories = categories;
         this.rides = rides;
         this.currencies = currencies;
         this.templates = templates;
-        this.count = count;
+
         generator = new TestDataGenerator(guidProvider);
     }
 
-    public void initialize() {
+    @Override
+    public void initialize(int count) {
         List<Category> cat = generator.createTestCategories(count);
         cat.forEach(categories::addRow);
 
