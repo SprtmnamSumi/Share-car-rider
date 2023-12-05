0c17620ec317f17ad0a3519396e5f9587fcd1619
Jan Šmíd <xsmid2@fi.muni.cz>
2023-12-05 23:09:16 +0100

    [CAR-40-export] Adds basic export tests, corrects BatchImporter

---
+++ src/test/java/cz/muni/fi/pv168/project/business/service/export/BatchImporterUnitTest.java
@@ -0,0 +1,52 @@
+package cz.muni.fi.pv168.project.business.service.export;
+
+import cz.muni.fi.pv168.project.export.BatchImporterCarRideJSON;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static cz.muni.fi.pv168.project.business.service.statistics.CarRideStatisticsUnitTests.createCarRideThree;
+import static cz.muni.fi.pv168.project.business.service.statistics.CarRideStatisticsUnitTests.createCarRideTwo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BatchImporterUnitTest
+{
+    private static final Path PROJECT_ROOT = Paths.get(System.getProperty("project.basedir", "")).toAbsolutePath();
+    private static final Path TEST_RESOURCES = PROJECT_ROOT.resolve(Path.of("src", "test", "resources"));
+
+    private final BatchImporterCarRideJSON batchImporterCarRideJSON = new BatchImporterCarRideJSON();
+
+    @Test
+    void importNoEmployees() {
+        Path importFilePath = TEST_RESOURCES.resolve("empty.json");
+        var carRides = batchImporterCarRideJSON.importData(importFilePath);
+
+        assertThat(carRides).isEmpty();
+    }
+
+    @Test
+    void singleEmployee() {
+        Path importFilePath = TEST_RESOURCES.resolve("single-carride.json");
+        var carRides = batchImporterCarRideJSON.importData(importFilePath);
+
+        assertThat(carRides)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactly(
+                        createCarRideTwo("c-2")
+                );
+    }
+
+    @Test
+    void multipleCarRides() {
+        Path importFilePath = TEST_RESOURCES.resolve("multiple-carrides.json");
+        var carRides = batchImporterCarRideJSON.importData(importFilePath);
+
+        assertThat(carRides)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsExactlyInAnyOrder(
+                        createCarRideTwo("c-2"),
+                        createCarRideThree("c-3")
+                );
+    }
+}
