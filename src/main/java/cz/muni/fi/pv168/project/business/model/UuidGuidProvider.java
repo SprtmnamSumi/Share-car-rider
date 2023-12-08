3072ea2f211257dd5a74b8f7df17fd3b240338bb
ManasComp <OndrejMan1@gmail.com>
2023-12-08 11:35:24 +0100

    random guid provider
    Please enter the commit message for your changes. Lines starting

--- src/main/java/cz/muni/fi/pv168/project/business/model/UuidGuidProvider.java
+++ src/main/java/cz/muni/fi/pv168/project/business/model/UuidGuidProvider.java
@@ -2,12 +2,10 @@ package cz.muni.fi.pv168.project.business.model;
 
 import com.google.inject.Singleton;
 import java.util.UUID;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Singleton
 
 class UuidGuidProvider implements GuidProvider {
-
     @Override
     public String newGuid() {
         return UUID.randomUUID().toString(); // TODO can be error
