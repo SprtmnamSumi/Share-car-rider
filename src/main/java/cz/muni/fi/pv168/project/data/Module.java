29345eab8bb8718a1eddf9c4ad58b283a3dcac54
marvav <41584620+marvav@users.noreply.github.com>
2023-12-08 12:02:22 +0100

    Added logging to actions, images, main

--- src/main/java/cz/muni/fi/pv168/project/data/Module.java
+++ src/main/java/cz/muni/fi/pv168/project/data/Module.java
@@ -1,11 +1,13 @@
 package cz.muni.fi.pv168.project.data;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
 
 public class Module extends AbstractModule {
     @Override
     protected void configure() {
+        bind(ImportInitializer.class);
         bind(EntityProvider.class).to(PrefilledEntityProvider.class);
         bind(new TypeLiteral<Initializer>() {
         }).to(DataInitializer.class);
